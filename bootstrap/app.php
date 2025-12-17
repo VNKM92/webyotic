ac5b4901324745e6c7e39e5b5dfbcc73899f5ea2
Vinay Maurya <68512218+VNKM92@users.noreply.github.com>
2025-12-18 01:49:24 +0530

    w
    w

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,20 +12,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-         // Register global middleware
-        $middleware->web(append: [
-            \App\Http\Middleware\EncryptCookies::class,
-            \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
-            \Illuminate\Session\Middleware\StartSession::class,
-            \Illuminate\View\Middleware\ShareErrorsFromSession::class,
-            \App\Http\Middleware\VerifyCsrfToken::class,
-            \Illuminate\Routing\Middleware\SubstituteBindings::class,
-        ]);
-
         // Register route middleware
         $middleware->alias([
-            'auth' => \App\Http\Middleware\Authenticate::class,
-            'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
             'admin' => \App\Http\Middleware\IsAdmin::class,
         ]);
     })
